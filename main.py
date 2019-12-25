413652ff2e993ac0d6367da60edbaf9f64d023d1
narhz <narhz@pm.me>
2019-12-25 09:49:43 -0600

    inital commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+import random
+from time import sleep
+
+
+
+while True:
+    rand_time = random.randint(300, 3600)
+    sleep(rand_time)
+
+    playsound('baby.mp3')
